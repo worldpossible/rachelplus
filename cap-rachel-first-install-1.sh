147c02f76efef81b627edc25bd6dc4a4b81e079c
Sam Kinch <spatiald@users.noreply.github.com>
2015-07-12 00:45:54 -0400

    Cleaned up code

--- cap-rachel-first-install-1.sh
+++ cap-rachel-first-install-1.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# cap-rachel-first-install-1.sh
+# FILE: cap-rachel-first-install-1.sh
+# ONELINER Download/Install: sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-first-install-1.sh -O - | bash 
 
 function print_error () {
     echo -e "\x1B[01;31m[-]\x1B[0m $1"
@@ -15,11 +16,12 @@ if [ "$(id -u)" != "0" ]; then
   exit 1
 fi
 
-print_status "[+] Updating CAP package repositories"
+echo; print_status "Updating CAP package repositories"
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 16126D3A3E5C1192
 apt-get update
+print_good "Done."
 
-print_status "[+] Repartitioning hard drive"
+echo; print_status "Repartitioning hard drive"
 sgdisk -p /dev/sda
 sgdisk -o /dev/sda
 parted -s /dev/sda mklabel gpt
@@ -27,10 +29,13 @@ sgdisk -n 1:2048:+20G -c 1:"preloaded" -u 1:77777777-7777-7777-7777-777777777777
 sgdisk -n 2:21G:+100G -c 2:"uploaded" -u 2:88888888-8888-8888-8888-888888888888 -t 2:8300 /dev/sda
 sgdisk -n 3:122G:-1M -c 3:"RACHEL" -u 3:99999999-9999-9999-9999-999999999999 -t 3:8300 /dev/sda
 sgdisk -p /dev/sda
+print_good "Done."
 
-print_status "[+] Adding /dev/sda3 into /etc/fstab"
+echo; print_status "Adding /dev/sda3 into /etc/fstab"
 echo -e "/dev/sda3\t/media/RACHEL\t\text4\tauto,nobootwait 0\t0" >> /etc/fstab
+print_good "Done."
 
-print_status "[+] I need to reboot; once rebooted, please run cap-rachel-first-install-2.sh"
-read -rsp $'Press any key to reboot...\n' -n1 key
+echo; print_status "[+] I need to reboot; once rebooted, please run the next download/install command."
+print_status "Rebooting in 10 seconds..." 
+sleep 10
 reboot
