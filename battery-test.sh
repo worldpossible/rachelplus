e74d4c268debb3449857545c13c271ff71a9385f
Sam Kinch <spatiald@users.noreply.github.com>
2015-07-15 13:45:33 -0400

    Update battery-test.sh
    Cleaned up script.  For some reason, script runs as desired during test but when the CAP would shut down due to battery failure, the logs weren't written correctly.

--- battery-test.sh
+++ battery-test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ##############################################################################
 # Hackers for Charity (http://hackersforcharity.org/education)
-# 
-# Description:  This file is used to test duration of various batteries 
-# running the device.  It will ask whether or not you wish to set the  
-# date/time, then create a file named in the LOG variable relative to  
+#
+# Description:  This file is used to test duration of various batteries
+# running the device.  It will ask whether or not you wish to set the
+# date/time, then create a file named in the LOG variable relative to
 # the current folder.
 #
-# Usage: Ensure script is executable: "chmod +x ./battery-test.sh" and 
+# Usage: Ensure script is executable: "chmod +x ./battery-test.sh" and
 # then start the script once you remove all but the battery as a power
 # source.  The script will continually update until power fails.  When you
 # reboot, you can then read the log file (default: duration-results) to
@@ -15,64 +15,89 @@
 ##############################################################################
 
 # Change the following, if desired
-VERSION=6 #script version
-LOG="duration-results"  #default log file
+VERSION=7 #script version
+LOGFILE="duration-results"  #default log filename
+LOG="$PWD/$LOGFILE" #full path to log file
 
+# Do not change anything below unless you know what you are doing
+
+# Create temp file
+TMP=`mktemp -t battery-test.tmpXXX`
+
+# Function that will remove the temp file on start; program continues to run until system shuts down
+function finish {
+	rm -rf "$TMP"
+}
+
+# Save the previous logfile, if found
+if [[ -f $LOG ]]; then
+	mv $LOG $LOG.bak
+	echo; echo "[+] Backup of previously run test saved to $LOG.bak"
+fi
+
+# Identify the script we are running
 echo; echo "### Battery Duration Test Script (sam@hfc) - Version $VERSION ###"; echo
-# Do not change anything after this line
+
 # Request if the user wants to set the log file
-echo "[!] Current log file name: $PWD/$LOG"
-read -r -p "[?] Do you want to rename your log file? [y/N] " response
-response=${response,,}
-if [[ $response =~ ^(yes|y)$ ]]
-then
-	echo "[?] What do you want to call your log file?"
+echo "[!] Current log file name: $LOG"
+echo; read -p "[?] Do you want to rename your log file? (y/n) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo; echo "[?] What is the full path to your log file?"
 	read LOG
-	echo "[+] Log file created: $PWD/$LOG" | tee -a $LOG
+	echo "[+] Log file created: $LOG" | tee -a $LOG
 else
-	echo "[-] Using default log file: $PWD/$LOG" | tee -a $LOG
+	echo; echo "[-] Using default log file: $LOG" | tee -a $LOG
 fi
 
 # Request info to set the current date/time
-echo; read -r -p "[?] Do you want to set the date/time?? [y/N] " response
-response=${response,,}
-if [[ $response =~ ^(yes|y)$ ]]; then
+echo; echo "[+] Current date/time:  "$(date)
+echo; read -p "[?] Do you want to set the date/time? (y/n) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
 	if [[ -f `which ntpdate` ]]; then
-		read -r -p "[?] Is this device connected to the internet? (y/n) " response
-		if [[ $response =~ ^(yes|y)$ ]]; then
+		echo; echo; read -p "[?] Is this device connected to the internet? (y/n) " -n 1 -r
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
 			echo "[+] ntpdate exists, attempting to set date/time automatically."
 			ntpdate time.apple.com
-			echo "[+] Done."
+			echo "[+] Time automatically set to: "$(date)
 		else
-			echo "[?] What is the current date/time (use this format --> Jul 5 08:10)?"
+			echo "[?] What is the current date/time (use this format --> Jul 5 08:10)? "
 			read DATE
 			sudo date -s "$DATE" >/dev/null 2>&1
 			echo "[+] Using manually entered date/time"
 		fi
 	fi
 else
-	echo "[+] Using system date/time"
+	echo; echo "[+] Using system date/time"
 fi
 
 # Sends initial test start comments to log file
-echo; echo "[+] Battery Duration Test Script - Version $VERSION" | tee $LOG
-STARTDATE=$(date +"%s")
-echo "[+] Test Started:  $(date -d @$STARTDATE)" | tee -a $LOG 
+echo; echo "[+] Battery Duration Test Script - Version $VERSION" > $LOG
+export STARTDATE=$(date +"%s")
+echo "[+] Test Started:  $(date -d @$STARTDATE)" >> $LOG
 
-# Monitors device for failure
-#while :; do sed -i '/Power failure/d' $LOG; FAILDATE=$(date); echo "[-] Power failure:  "$FAILDATE >> $LOG; sleep 5; done
-echo "[!] Monitoring for battery failure..."
-echo "[!] Ctrl-C to exit"
-while :; \
-do sed -i '/Power failure/d' $LOG; \
-sed -i '/Battery lasted/d' $LOG; \
-FAILDATE=$(date +"%s"); \
-echo "[-] Power failure:  "$(date -d @$FAILDATE) >> $LOG; \
-DIFF=$(($FAILDATE-$STARTDATE)); \
-REST=$(($DIFF%3600));
-HOURS=$((($DIFF-$REST)/3600));
-SECONDS=$(($REST%60));
-MINUTES=$((($REST-$SECONDS)/60));
-echo "[!] Battery lasted:  $HOURS hours, $MINUTES minutes, $SECONDS seconds" >> $LOG; \
-sleep 5; \
+# Create tmp file that will monitor battery in background
+
+sed "s,%LOG%,$LOG,g" >$TMP << 'EOF'
+#!/bin/bash
+while :; do
+	sed -i '/Power failure/d' %LOG%
+	sed -i '/Battery lasted/d' %LOG%
+
+	FAILDATE=$(date +"%s")
+	echo "[-] Power failure:  "$(date -d @$FAILDATE) >> %LOG%
+	DIFF=$(($FAILDATE-$STARTDATE))
+	REST=$(($DIFF%3600))
+	HOURS=$((($DIFF-$REST)/3600))
+	SECONDS=$(($REST%60))
+	MINUTES=$((($REST-$SECONDS)/60))
+	echo "[!] Battery lasted:  $HOURS hours, $MINUTES minutes, $SECONDS seconds" >> %LOG%
+	sleep 5
 done
+EOF
+
+# Start tmp file to monitor for battery failure
+bash $TMP&
+sleep 2
+trap finish EXIT
+echo "[!] Monitoring for battery failure...please remember to disconnect the power to the device under testing."
+echo
