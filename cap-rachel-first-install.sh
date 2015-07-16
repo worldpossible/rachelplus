eede65463fc40d7649d52ee84b5726c1bc2c1b2b
Sam Kinch <spatiald@users.noreply.github.com>
2015-07-16 00:01:03 -0400

    Update cap-rachel-first-install.sh

--- cap-rachel-first-install.sh
+++ cap-rachel-first-install.sh
@@ -26,28 +26,36 @@ fi
 # Delete previous setup commands from the /etc/rc.local
 sudo sed -i '/cap-rachel/d' /etc/rc.local
 
+# Add header/date/time to install log file
+echo; print_good "RACHEL CAP Install - Started $(date)" | tee $RACHELLOG
+
+echo; print_status "Downloading RACHLEL install scripts for CAP" | tee -a $RACHELLOG
 # Download additional scripts to /root
-echo; print_status "Downloading RACHLEL install scripts for CAP"
+echo; print_status "Downloading RACHLEL install scripts for CAP" | tee -a $RACHELLOG
 ## cap-rachel-first-install-1.sh
-sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-first-install-1.sh -O /root/cap-rachel-first-install-1.sh
+sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-first-install-1.sh -O /root/cap-rachel-first-install-1.sh | tee -a $RACHELLOG
+print_good "Done." | tee -a $RACHELLOG
 ## cap-rachel-first-install-2.sh
-sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-first-install-2.sh -O /root/cap-rachel-first-install-2.sh
+sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-first-install-2.sh -O /root/cap-rachel-first-install-2.sh | tee -a $RACHELLOG
+print_good "Done." | tee -a $RACHELLOG
 ## cap-rachel-first-install-3.sh
-sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-first-install-3.sh -O /root/cap-rachel-first-install-3.sh
+sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-first-install-3.sh -O /root/cap-rachel-first-install-3.sh | tee -a $RACHELLOG
+print_good "Done." | tee -a $RACHELLOG
 ## cap-rachel-setwanip-install.sh
-sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-setwanip-install.sh -O /root/cap-rachel-setwanip-install.sh
-echo; print_good "Downloads complete"
+sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-setwanip-install.sh -O /root/cap-rachel-setwanip-install.sh | tee -a $RACHELLOG
+print_good "Done." | tee -a $RACHELLOG
+echo; print_good "All downloads complete" | tee -a $RACHELLOG
 
 # Show location of the log file
-echo; print_status "Location of RACHEL install log file:"
-echo "$RACHELLOG"
+echo; print_status "Location of RACHEL install log file:" | tee -a $RACHELLOG
+echo "$RACHELLOG" | tee -a $RACHELLOG
 
 # Ask if you are ready to install
 echo; read -p "Are you ready to start the install? " -n 1 -r <&1
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	echo; print_status "Starting first install script..."
+	echo; print_status "Starting first install script..." | tee -a $RACHELLOG
 	bash /root/cap-rachel-first-install-1.sh
 else
-	echo; print_error "User requests not to continue...not installing."
+	echo; print_error "User requests not to continue...exiting at $(date)" | tee -a $RACHELLOG
 	echo; exit 1
 fi
