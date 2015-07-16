2a8730e55b1e4e727d0230ea96d1f8110eca763f
Sam Kinch <spatiald@users.noreply.github.com>
2015-07-16 09:57:46 -0400

    Update cap-rachel-first-install-1.sh

--- cap-rachel-first-install-1.sh
+++ cap-rachel-first-install-1.sh
@@ -2,10 +2,12 @@
 # FILE: cap-rachel-first-install-1.sh
 # ONELINER Download/Install: sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-first-install-1.sh -O - | bash 
 
-# Everything below will go to the file '/var/log/rachel-install.log'
-#exec 3>&1 4>&2
-#trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>>/var/log/rachel-install.log 2>&1
+# Everything below will go to this log directory
+RACHELLOGDIR="var/log/RACHEL"
+RACHELLOGFILE="rachel-install.tmp"
+RACHELLOG="$RACHELLOGDIR/$RACHELLOGFILE"
+
+exec 1>> $RACHELLOG 2>&1
 
 function print_good () {
     echo -e "\x1B[01;32m[+]\x1B[0m $1"
