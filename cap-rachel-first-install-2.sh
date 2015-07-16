1d1fda2506a9d564422c3a4a4bd2c4cb5da46004
Sam Kinch <spatiald@users.noreply.github.com>
2015-07-16 09:27:34 -0400

    Update cap-rachel-first-install-2.sh

--- cap-rachel-first-install-2.sh
+++ cap-rachel-first-install-2.sh
@@ -5,7 +5,7 @@
 # Everything below will go to the file '/var/log/rachel-install.log'
 #exec 3>&1 4>&2
 #trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>>/var/log/rachel-install.log 2>&1
+RACHELLOG="/var/log/rachel-install.log"
 
 function print_good () {
     echo -e "\x1B[01;32m[+]\x1B[0m $1"
@@ -26,24 +26,24 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # Add header/date/time to install log file
-echo; print_good "RACHEL CAP Install - Script 2 started at $(date)"
+echo; print_good "RACHEL CAP Install - Script 2 started at $(date)" | tee -a $RACHELLOG
 
 # Delete previous setup commands from the /etc/rc.local
-sudo sed -i '/cap-rachel/d' /etc/rc.local
+sudo sed -i '/cap-rachel/d' /etc/rc.local >> $RACHELLOG 2>&1
 
 # Create the new filesystems so we can write files to them
-echo; print_status "Creating filesystems"
-mkfs.ext4 -L "preloaded" -U 77777777-7777-7777-7777-777777777777 /dev/sda1
-mkfs.ext4 -L "uploaded" -U 88888888-8888-8888-8888-888888888888 /dev/sda2
-mkfs.ext4 -L "RACHEL" -U 99999999-9999-9999-9999-999999999999 /dev/sda3
-print_good "Done."
+echo; print_status "Creating filesystems" | tee -a $RACHELLOG
+mkfs.ext4 -L "preloaded" -U 77777777-7777-7777-7777-777777777777 /dev/sda1 >> $RACHELLOG 2>&1
+mkfs.ext4 -L "uploaded" -U 88888888-8888-8888-8888-888888888888 /dev/sda2 >> $RACHELLOG 2>&1
+mkfs.ext4 -L "RACHEL" -U 99999999-9999-9999-9999-999999999999 /dev/sda3 >> $RACHELLOG 2>&1
+print_good "Done." | tee -a $RACHELLOG
 
 # Add lines to /etc/rc.local that will start the next script to run on reboot
-sudo sed -i '$e echo "bash \/root\/cap-rachel-first-install-3.sh&"' /etc/rc.local
+sudo sed -i '$e echo "bash \/root\/cap-rachel-first-install-3.sh&"' /etc/rc.local >> $RACHELLOG 2>&1
 
 # Reboot
-echo; print_good "RACHEL CAP Install - Script 2 ended at $(date)"
-echo; print_status "I need to reboot; once rebooted, please run the next download/install command."
-print_status "Rebooting in 10 seconds..." 
+echo; print_good "RACHEL CAP Install - Script 2 ended at $(date)" | tee -a $RACHELLOG
+echo; print_status "I need to reboot; once rebooted, please run the next download/install command." | tee -a $RACHELLOG
+print_status "Rebooting in 10 seconds..." | tee -a $RACHELLOG
 sleep 10
-reboot
+reboot >> $RACHELLOG 2>&1
