2cbf6ad6e4e44cad18d01a8ed25023bc81acbed6
Sam Kinch <spatiald@users.noreply.github.com>
2015-07-17 12:43:15 -0400

    Removed the need to update server IP

---
+++ captiveportal-redirect.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>RACHEL</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="common.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<!--[if IE]><script type="text/javascript" src="css3-multi-column.min.js"></script><![endif]-->
+</head>
+<body link="#000000" vlink="#000000" alink="#377211">
+<div style='text-align:center'>
+<a href="index.php" target="_blank">
+        <img src="art/rachel_banner.png" />
+</a>
+<p><h2>The World's best educational resources, neatly packaged together for download and distribution in places without internet.</h2></p>
+</br>
+<h3>To access RACHEL content, click the logo above or open your web browser and go to <?php echo $_SERVER["SERVER_ADDR"];?></h3>
+</br><hr></br>
+<b>To create a RACHEL link on your desktop, follow these three simple steps using Firefox, Chrome or Internet Explorer (IE):</b></br>
+1) Resize your Web browser so you can see the browser and your desktop in the same screen.</br>
+2) Left click the icon located to the left side of the address bar. This is where you see the full URL to the website.</br>
+3) Continue to hold down the mouse button and drag the icon to your desktop. This creates the shortcut.
+</div>
+</body>
+</html>
