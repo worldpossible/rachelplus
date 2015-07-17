952992f73c789fd32d8eafd9b00e6895486b3b3b
Sam Kinch <spatiald@users.noreply.github.com>
2015-07-17 12:07:39 -0400

    Create cap-rachel-post-install.sh
    VERY VERY VERY BETA

---
+++ cap-rachel-post-install.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# VERY VERY VERY VERY VERY BETA
+
+# FILE: cap-rachel-post-install.sh
+# ONELINER Download/Install: sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-post-install.sh -O - | bash 
+
+# Everything below will go to this log directory
+RACHELLOGDIR="/var/log/RACHEL"
+mkdir -p $RACHELLOGDIR
+RACHELLOGFILE="rachel-install.tmp"
+RACHELLOG="$RACHELLOGDIR/$RACHELLOGFILE"
+
+function print_good () {
+    echo -e "\x1B[01;32m[+]\x1B[0m $1"
+}
+
+function print_error () {
+    echo -e "\x1B[01;31m[-]\x1B[0m $1"
+}
+
+function print_status () {
+    echo -e "\x1B[01;35m[*]\x1B[0m $1"
+}
+
+function print_question () {
+    echo -e "\x1B[01;33m[?]\x1B[0m $1"
+}
+
+print_error "This script is too beta at the moment, exiting."
+exit 1
+
+# Check root
+if [ "$(id -u)" != "0" ]; then
+	print_error "This step must be run as root; sudo password is 123lkj"
+	exit 1
+fi
+
+# Add header/date/time to install log file
+echo; print_good "RACHEL CAP Post-Install Script - Version 1" | tee $RACHELLOG
+print_good "Install started: $(date)" | tee -a $RACHELLOG
+
+# Reconfigure RACHEL files
+echo; print_question "Before running this script, you need to have copied the basic RACHEL content to" | tee -a $RACHELLOG
+echo "  /media/RACHEL/rachel on the Intel CAP.  You should see the RACHEL index.php file at /media/RACHEL/rachel/index.php" | tee -a $RACHELLOG
+echo; echo "  If you are not ready, answer 'N', copy over the data and then run this script again." | tee -a $RACHELLOG
+
+read -p "Are you ready to update the RACHEL files? " -r <&1
+if [[ $REPLY =~ ^[yY][eE][sS]|[yY]$ ]]; then
+	echo; print_status "Fixing index.php." | tee -a $RACHELLOG
+	# Redirect LOCAL CONTENT link to Intel CAP's content manager site on port 8090
+	sed -i 's|<li><a href="local-frameset.html">LOCAL CONTENT</a></li>|<li><a href="./" onclick="javascript:event.target.port=8090" target="_blank">LOCAL CONTENT</a></li>|g' /media/RACHEL/rachel/index.php
+	sed -i 's|<div id="ip">http://<?php echo gethostbyname(gethostname()); ?>/</div>|<div id="ip">http://192.168.88.1</div>|g' /media/RACHEL/rachel/index.php
+else
+	echo; print_error "User requests not to continue...exiting at $(date)" | tee -a $RACHELLOG
+	# Deleting the install script commands
+	rm -f /root/cap-rachel-* $RACHELLOG
+	echo; exit 1
+fi
