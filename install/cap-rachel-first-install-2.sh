8aa9b75771e1298e43b147b47c02f178bfd95c25
Jeremy Schwartz <jeremy@worldpossible.org>
2015-07-20 14:00:22 -0700

    Update cap-rachel-first-install-2.sh

--- install/cap-rachel-first-install-2.sh
+++ install/cap-rachel-first-install-2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # FILE: cap-rachel-first-install-2.sh
-# ONELINER Download/Install: wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-first-install-2.sh -O - | bash 
+# ONELINER Download/Install: wget https://github.com/rachelproject/rachelplus/raw/master/install/cap-rachel-first-install-2.sh -O - | bash 
 
 # Everything below will go to this log directory
 RACHELLOGDIR="/var/log/RACHEL"
