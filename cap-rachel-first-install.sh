77c2bc70ba118bee12eacaf8c2c535f39f1f3d0b
Sam Kinch <spatiald@users.noreply.github.com>
2015-07-23 18:00:28 -0400

    Added back in ability to choose pkg repo

--- cap-rachel-first-install.sh
+++ cap-rachel-first-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # FILE: cap-rachel-first-install.sh
-# ONELINER Download/Install: sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-first-install.sh -O - | bash 
+# ONELINER Download/Install: sudo wget https://github.com/rachelproject/rachelplus/raw/master/cap-rachel-first-install.sh -O /root/cap-rachel-first-install.sh; bash cap-rachel-first-install.sh
 
 # Everything below will go to this log directory
 RACHELLOGDIR="/var/log/RACHEL"
@@ -33,8 +33,8 @@ function print_question () {
 
 # Check root
 if [ "$(id -u)" != "0" ]; then
-	print_error "This step must be run as root; sudo password is 123lkj"
-	exit 1
+    print_error "This step must be run as root; sudo password is 123lkj"
+    exit 1
 fi
 
 # Add header/date/time to install log file
@@ -45,10 +45,10 @@ print_good "Install started: $(date)" | tee -a $RACHELLOG
 WGET=`which wget`
 $WGET -q --tries=10 --timeout=5 --spider http://google.com 1>> $RACHELLOG 2>&1
 if [[ $? -eq 0 ]]; then
-	echo; print_good "Internet connected...continuing install." | tee -a $RACHELLOG
+    echo; print_good "Internet connected...continuing install." | tee -a $RACHELLOG
 else
-	echo; print_error "No internet connectivity; connect to the internet and try again."
-	exit 1
+    echo; print_error "No internet connectivity; connect to the internet and try again."
+    exit 1
 fi
 
 # Fix hostname issue in /etc/hosts
@@ -61,7 +61,6 @@ echo; print_status "Delete previous RACHEL setup commands from /etc/rc.local" | 
 sudo sed -i '/cap-rachel/d' /etc/rc.local 1>> $RACHELLOG 2>&1
 print_good "Done." | tee -a $RACHELLOG
 
-: <<'COMMENT'
 ## sources.list - replace the package repos for more reliable ones (/etc/apt/sources.list)
 echo; print_status "Locations for downloading packages:"
 echo "    US) United States"
@@ -69,7 +68,7 @@ echo "    UK) United Kingdom"
 echo "    SG) Singapore"
 echo "    CN) China (CAP Manufacturer's Site)"
 echo; print_question "For the package downloads, select the location nearest you? "
-select class in "US" "UK" "SG" "CN"; do <&1
+select class in "US" "UK" "SG" "CN"; do
         case $class in
         # US
         US)
@@ -104,12 +103,13 @@ select class in "US" "UK" "SG" "CN"; do <&1
         ;;
         esac
 done
-COMMENT
 
+: <<'COMMENT'
 # For the package downloads, we are setting the default of UK until I can fix the CASE statemetn above
 echo; print_status "Downloading packages from the United Kingdom."
 sudo wget https://github.com/rachelproject/rachelplus/raw/master/sources.list/sources-uk.list -O $SOURCESLIST 1>> $RACHELLOG 2>&1
 print_good "Done."
+COMMENT
 
 # Download additional scripts to /root
 echo; print_status "Downloading RACHEL install scripts for CAP" | tee -a $RACHELLOG
@@ -122,16 +122,16 @@ sudo wget https://github.com/rachelproject/rachelplus/raw/master/install/cap-rac
 ## lighttpd.conf - RACHEL version (I don't overwrite at this time due to other dependencies)
 sudo wget https://github.com/rachelproject/rachelplus/raw/master/lighttpd.conf -O $LIGHTTPDFILE 1>> $RACHELLOG 2>&1
 if [[ -s $FILE1 && -s $FILE2 && -s $FILE3 && -s $LIGHTTPDFILE && -s $SOURCESLIST ]]  1>> $RACHELLOG 2>&1; then
-	print_good "Done." | tee -a $RACHELLOG
+    print_good "Done." | tee -a $RACHELLOG
 else
-	print_error "One or more files did not download correctly; check log file (/var/log/RACHEL/rachel-install.tmp) and try again." | tee -a $RACHELLOG
-	echo "The following files should have downloaded to /root:" | tee -a $RACHELLOG
-	echo "cap-rachel-first-install-1.sh" | tee -a $RACHELLOG
-	echo "cap-rachel-first-install-2.sh" | tee -a $RACHELLOG
-	echo "cap-rachel-first-install-3.sh" | tee -a $RACHELLOG
-	echo "lighttpd.conf" | tee -a $RACHELLOG
-	echo "sources.list" | tee -a $RACHELLOG
-	echo; exit 1
+    print_error "One or more files did not download correctly; check log file (/var/log/RACHEL/rachel-install.tmp) and try again." | tee -a $RACHELLOG
+    echo "The following files should have downloaded to /root:" | tee -a $RACHELLOG
+    echo "cap-rachel-first-install-1.sh" | tee -a $RACHELLOG
+    echo "cap-rachel-first-install-2.sh" | tee -a $RACHELLOG
+    echo "cap-rachel-first-install-3.sh" | tee -a $RACHELLOG
+    echo "lighttpd.conf" | tee -a $RACHELLOG
+    echo "sources.list" | tee -a $RACHELLOG
+    echo; exit 1
 fi
 
 # Show location of the log file
@@ -144,12 +144,12 @@ echo "It will update the contentshell files with the latest ones from GitHub." |
 
 echo; read -p "Are you ready to start the install? (y/n) " -r <&1
 if [[ $REPLY =~ ^[yY][eE][sS]|[yY]$ ]]; then
-	echo; print_status "Starting first install script...please wait patiently (about 30 secs) for first reboot." | tee -a $RACHELLOG
-	print_status "The entire script (with reboots) takes 2-5 minutes." | tee -a $RACHELLOG
-	bash /root/cap-rachel-first-install-1.sh
+    echo; print_status "Starting first install script...please wait patiently (about 30 secs) for first reboot." | tee -a $RACHELLOG
+    print_status "The entire script (with reboots) takes 2-5 minutes." | tee -a $RACHELLOG
+    bash /root/cap-rachel-first-install-1.sh
 else
-	echo; print_error "User requests not to continue...exiting at $(date)" | tee -a $RACHELLOG
-	# Deleting the install script commands
-	rm -f /root/cap-rachel-* $RACHELLOG
-	echo; exit 1
+    echo; print_error "User requests not to continue...exiting at $(date)" | tee -a $RACHELLOG
+    # Deleting the install script commands
+    rm -f /root/cap-rachel-* $RACHELLOG
+    echo; exit 1
 fi
